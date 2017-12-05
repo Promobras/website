49770b0db0fdba607dcd8921b08790c4aed962b4
Daniel Muller <github@daniel.ctrlaltdel.ch>
2017-12-06 00:29:55 +0800

    Add some UI

---
+++ src/js/hero.js
@@ -0,0 +1,34 @@
+var $falseHeader = $('.false-header')
+var $shadower = $('.shadower')
+var $fixedHead = $('.fixed-head')
+var $menuLi   = $('.menu ul li')
+var stickyHeight = $falseHeader.offset().top
+
+$(window).scroll(function() {
+  
+  var wScroll = $(this).scrollTop()
+  var headScroll = (-wScroll / 2)
+  var faderScroll = (wScroll / 400)
+  var fadeToColor = Math.min(faderScroll, 1)
+
+  $shadower.css({
+    opacity: fadeToColor
+  })
+
+  $fixedHead.css({
+    backgroundPosition: '50%' + (headScroll) + 'px'
+  })
+
+  if (wScroll >= stickyHeight) {
+    $falseHeader.addClass('clipped')
+  } else {
+    $falseHeader.removeClass('clipped')
+  }
+ 
+})
+
+
+$menuLi.click(function() {
+  $menuLi.removeClass('current')
+  $(this).addClass('current')
+})
