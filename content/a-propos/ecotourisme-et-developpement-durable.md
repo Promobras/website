d4fa5277b0d33294663da84b052a55d481b3c1bc
Daniel Muller <github@daniel.ctrlaltdel.ch>
2017-12-27 01:22:49 +0800

    improve photoswipe

--- content/a-propos/ecotourisme-et-developpement-durable.md
+++ content/a-propos/ecotourisme-et-developpement-durable.md
@@ -8,17 +8,17 @@ tagline: null
 weight: 1
 ---
 ### L'écotourisme ou le tourisme vert
-{{% cl_img class="left w30" src="/uploads/2828.jpg" %}}
+{{% cl_img class="left w30" lightbox="g1" src="/uploads/2828.jpg" %}}
 
 est une forme de tourisme alternatif centré sur la découverte de la nature (écosystèmes, mais aussi agrosystèmes et tourisme rural), voire d’écologie urbaine (jardins écologiques, espaces verts écologiques, réserves naturelles urbaines). Il est généralement pratiqué en petites groupes ou individuellement. Les voyageurs pratiquant l’écotourisme partent à la découverte d’espaces naturels. Observation de la faune, contemplation des paysages, étude de la flore, pratique de la randonnée … sont des éléments essentiels d’un voyage éco touristique. Les caractéristiques d’un voyage éco touristique, comparé au tourisme de masse, sont la limitation de l’empreinte écologique, l’éducation environnementale, le bien-être des populations locales et le soutien aux programmes de conservation de la biodiversité.
 
 ### Développement durable
-{{% cl_img class="left w30" src="/uploads/2831.jpg" %}}
+{{% cl_img class="left w30" lightbox="g1" src="/uploads/2831.jpg" %}}
 
 Alors que les premières définitions de l’écotourisme mettaient l’accent sur une proximité avec la nature recherchée par les touristes, les définitions plus récentes ont plutôt cherché à mettre en lumière une variété de principes associés au concept de développement durable. On admet actuellement que l’écotourisme englobe les principes du tourisme durable en ce qui concerne les impacts de cette activité sur l’économie, la société et l’environnement. L’écotourisme ne se résume pas à la découverte des paysages, de la faune et de la flore d’une région, mais aussi à celle de ses habitants et vise à sensibiliser aussi bien les voyageurs que les populations locales de la nécessité de préserver l’environnement.
 
 ### Nature, éducation et durabilité
-{{% cl_img class="left w30" src="/uploads/2534.jpg" %}}
+{{% cl_img class="left w30" lightbox="g1" src="/uploads/2534.jpg" %}}
 
 C’est ainsi que l’écotourisme responsable contribue activement à la protection du patrimoine naturel et culturel, en proposant aux visiteurs une interprétation de ce patrimoine, tout en incluant les communautés locales et indigènes dans sa planification, son développement et son exploitation et en contribuant à leur bien-être.
 En résumé, l’écotourisme répond à trois critères :
--- src/js/pswp.js
+++ src/js/pswp.js
@@ -1,33 +1,47 @@
 /* global PhotoSwipe, PhotoSwipeUI_Default */
-var openPhotoSwipe = () => {
+var openPhotoSwipe = (e) => {
+  let group = $(e.target).parent().attr('data-lightbox')
   var pswpElement = document.querySelectorAll('.pswp')[0]
 
   // build items array
-  var items = [
-    {
-      src: '/uploads/2828.jpg',
-      w: 800,
-      h: 600
-    },
-    {
-      src: '/uploads/2831.jpg',
-      w: 800,
-      h: 600
-    },
-    {
-      src: '/uploads/2534.jpg',
-      w: 800,
-      h: 600
+  var items = []
+  let images = $('div.img[data-lightbox=' + group + ']>img')
+  for (let i = 0; i < images.length; i++) {
+    let value = images[i]
+    let src = $(value).attr('data-src')
+    if (typeof src === 'undefined') {
+      src = value.src
     }
-  ]
-
+    console.log(src)
+    let item = {
+      large: {
+        src: src.replace('w_auto', 'w_1600'),
+        w: 1600,
+        h: 1200
+      },
+      medium: {
+        src: src.replace('w_auto', 'w_800'),
+        w: 800,
+        h: 600
+      },
+      small: {
+        src: src.replace('w_auto', 'w_400'),
+        w: 400,
+        h: 300
+      },
+      msrc: value.src
+    }
+    items.push(item)
+  }
   // define options (if needed)
   var options = {
   // optionName: 'option value'
   // for example:
     index: 0, // start at first slide
-    mainClass: 'pswp--minimal--dark',
     barsSize: {top: 0, bottom: 0},
+    closeOnScroll: false,
+    showHideOpacity: true,
+    preload: [3, 3],
     captionEl: false,
     fullscreenEl: true,
     shareEl: false,
@@ -38,11 +52,67 @@ var openPhotoSwipe = () => {
 
   // Initializes and opens PhotoSwipe
   var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options)
-  gallery.listen('destroy', () => {
-    $('#header').css('display', '')
+  var realViewportWidth
+  var useImages = ''
+  var firstResize = true
+  var imageSrcWillChange
+
+  gallery.listen('beforeResize', function () {
+    // gallery.viewportSize.x - width of PhotoSwipe viewport
+    // gallery.viewportSize.y - height of PhotoSwipe viewport
+    // window.devicePixelRatio - ratio between physical pixels and device independent pixels (Number)
+    //                          1 (regular display), 2 (@2x, retina) ...
+
+    // calculate real pixels when size changes
+    realViewportWidth = gallery.viewportSize.x * window.devicePixelRatio
+
+    // Code below is needed if you want image to switch dynamically on window.resize
+
+    // Find out if current images need to be changed
+    console.log(realViewportWidth)
+    if (useImages !== 'small' && realViewportWidth < 400) {
+      useImages = 'small'
+      imageSrcWillChange = true
+    } else if (useImages !== 'medium' && realViewportWidth >= 400 && realViewportWidth < 800) {
+      useImages = 'medium'
+      imageSrcWillChange = true
+    } else if (useImages !== 'large' && realViewportWidth >= 800) {
+      useImages = 'large'
+      imageSrcWillChange = true
+    }
+
+    // Invalidate items only when source is changed and when it's not the first update
+    if (imageSrcWillChange && !firstResize) {
+      // invalidateCurrItems sets a flag on slides that are in DOM,
+      // which will force update of content (image) on window.resize.
+      gallery.invalidateCurrItems()
+    }
+
+    if (firstResize) {
+      firstResize = false
+    }
+
+    imageSrcWillChange = false
+  })
+
+  // gettingData event fires each time PhotoSwipe retrieves image source & size
+  gallery.listen('gettingData', function (index, item) {
+    // Set image source & size based on real viewport width
+    item.src = item[useImages].src
+    item.w = item[useImages].w
+    item.h = item[useImages].h
+
+    // It doesn't really matter what will you do here,
+    // as long as item.src, item.w and item.h have valid values.
+    //
+    // Just avoid http requests in this listener, as it fires quite often
   })
   gallery.init()
+  gallery.listen('destroy', () => {
+    $('#header').css('display', '')
+    $('#navButton').css('display', 'block')
+  })
   $('#header').css('display', 'none')
+  $('#navButton').css('display', 'none')
 }
-
-document.getElementById('btn').onclick = openPhotoSwipe
+$('div.img[data-lightbox]>img').click((e) => openPhotoSwipe(e))
