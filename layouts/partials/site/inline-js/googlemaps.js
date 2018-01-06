408d9e8925a4b1cedf5455c4a0374dd2b919fe6d
Daniel Muller <github@daniel.ctrlaltdel.ch>
2018-01-07 00:26:43 +0800

    Update map inclusion

--- layouts/partials/site/inline-js/googlemaps.js
+++ layouts/partials/site/inline-js/googlemaps.js
@@ -6,21 +6,21 @@ function initMaps () {
   /* eslint-enable no-unused-vars */
   $('.map').each(function (index) {
     var coords = {lat: -12.6346871, lng: -58.2792325}
-    var showMarker = false
+    var displayMarker = false
     var zoom = 4
     var lat = parseFloat($(this).attr('data-lat'))
     var lng = parseFloat($(this).attr('data-lng'))
     var zoomAttr = parseInt($(this).attr('data-zoom'))
+    var showMarker = $(this).attr('data-showmarker')
     if (lat && lng) {
       coords = {lat: lat, lng: lng}
-      showMarker = true
+      displayMarker = true
       if (zoomAttr) {
         zoom = zoomAttr
       }
     }
 
     var mapTypeId = $(this).attr('data-maptype')
-    console.log(mapTypeId)
     if (!mapTypeId) {
       mapTypeId = 'roadmap'
     }
@@ -33,44 +33,50 @@ function initMaps () {
       center: coords,
       mapTypeId: mapTypeId
     })
-    if (showMarker) {
+    if (showMarker && displayMarker) {
       marker[index] = new google.maps.Marker({
         position: coords,
         map: map[index]
       })
     }
 
-    var kmlLayer = new google.maps.KmlLayer({
-      url: 'https://www.google.com/maps/d/u/0/kml?mid=1aud-1M9f3UURW3IakSwaY5HU1Tg',
-      suppressInfoWindows: false,
-      map: map[index]
-    })
-    console.log(kmlLayer)
-
     var placeId = $(this).attr('data-placeid')
     if (placeId) {
       var geocoder = new google.maps.Geocoder()
       geocoder.geocode({'placeId': placeId}, function (results, status) {
-        console.log(results)
-        if (status === 'OK' && status[0]) {
+        if (status === 'OK' && results[0]) {
+          console.log(results[0])
           if (zoomAttr) {
             map[index].setZoom(zoomAttr)
           }
           map[index].setCenter(results[0].geometry.location)
-          if (marker[index]) {
-            marker[index].setPlace({
-              placeId: placeId,
-              location: results[0].geometry.location
-            })
-          } else {
-            marker[index] = new google.maps.Marker({
-              map: map[index],
-              placeId: placeId,
-              position: results[0].geometry.location
-            })
+          if (showMarker) {
+            if (marker[index]) {
+              marker[index].setPlace({
+                placeId: placeId,
+                location: results[0].geometry.location
+              })
+            } else {
+              marker[index] = new google.maps.Marker({
+                map: map[index],
+                placeId: placeId,
+                position: results[0].geometry.location
+              })
+            }
           }
         }
       })
+    }
+    var mapId = $(this).attr('data-mapid')
+    if (mapId) {
+      var preserveViewport = (showMarker || placeId)
+      var kmlLayer = new google.maps.KmlLayer({
+        url: 'https://www.google.com/maps/d/u/0/kml?mid=' + mapId,
+        suppressInfoWindows: false,
+        map: map[index],
+        preserveViewport: preserveViewport
+      })
+      console.log('kmlLayer', kmlLayer)
     }
   })
 }
